ed5c3d39332d0ee3ea608e6440203ddc6082464f
albuq <barbara.albuquerque@usp.br>
2016-10-17 08:10:15 -0200

    tabs pt 2
    Reserva de classes para banco de dados, conteudo da tela de exibição das checklists e tags

---
+++ app/src/main/java/com/weebly/taggtracker/tagtracker/BancoDeDados.java
@@ -0,0 +1,11 @@
+package com.weebly.taggtracker.tagtracker;
+
+/**
+ * Created by Barbara on 17/10/2016.
+ */
+
+public final class BancoDeDados  {
+     //Um bom link: https://developer.android.com/training/basics/data-storage/databases.html?hl=pt-br#DbHelper
+
+
+}
--- app/src/main/java/com/weebly/taggtracker/tagtracker/tela_inicial.java
+++ app/src/main/java/com/weebly/taggtracker/tagtracker/tela_inicial.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -23,9 +22,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-import static android.R.drawable.ic_input_add;
-import static android.R.drawable.ic_menu_close_clear_cancel;
-import static java.sql.DriverManager.println;
 
 
 public class tela_inicial extends AppCompatActivity
@@ -33,7 +29,7 @@ public class tela_inicial extends AppCompatActivity
 
     private NfcAdapter mNfcAdapter;
     private Dialog dialog;
-    private boolean menuExpandido;
+  //  private boolean menuExpandido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
