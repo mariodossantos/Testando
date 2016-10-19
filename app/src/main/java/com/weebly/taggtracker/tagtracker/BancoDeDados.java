8696ebce4ed307e69492e1041ca348681a1fb530
albuq <barbara.albuquerque@usp.br>
2016-10-19 15:14:24 -0200

    refazendo o BD pt 1

--- app/src/main/java/com/weebly/taggtracker/tagtracker/BancoDeDados.java
+++ app/src/main/java/com/weebly/taggtracker/tagtracker/BancoDeDados.java
@@ -72,7 +72,7 @@ public final class BancoDeDados  {
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // This database is only a cache for online data, so its upgrade policy is
             // to simply to discard the data and start over
-            db.execSQL(SQL_DELETE_ENTRIES);
+            db.execSQL(SQL_DELETE_ASSOCIA);
             onCreate(db);
         }
         public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
